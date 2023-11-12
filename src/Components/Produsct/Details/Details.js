535bb7fcdbe2cf25e09bef11b1732c400f488de6
bekazmr <bekazmr@gmail.com>
2023-11-12 11:56:49 +0600

    btn modify

--- src/Components/Produsct/Details/Details.js
+++ src/Components/Produsct/Details/Details.js
@@ -4,21 +4,36 @@ import HocWrap from '../../../hoc/HocWrap';
 import { Avatar, Card } from "antd";      
 import { PRODUCTAPI } from '../../configi/config';
 import fetchHoc from '../../../hoc/fetchHoc';
+import s from "./Detail.module.css"
 const Details = ({state}) => {
    const {id} = useParams();
   const navigate = useNavigate();
-  console.log(state);
+  const handleRowClick = (id) => {
+    navigate(`/details/${+id+1}`);
+  };
+  const handlePrev = (id) => {
+    navigate(`/details/${+id-1}`);
+  };
    return (
     <HocWrap>
-         <Card title="Todos Details" bordered={true}
-         cover={
-            <img
-              alt={state.category}
-              src={state.image}
-            />
-          }>
-           <h3>ID: {id}</h3>
-         </Card>
+     <div className={s.deatails__wrapper}>
+           <Card title={state.title} bordered={true}
+           cover={
+              <img className={s.detail__img}
+                alt={state.category}
+                src={state.image}
+              />
+            }>
+             <h3>Price: {`${state.price}$`}</h3>
+             <h3>ID: {id}</h3>
+     <div className={s.deatails__btn__wrapper}>
+               <button onClick={()=>{navigate("/")}}>BACK</button>
+          <button onClick={()=>handlePrev(id)}>PREV</button>
+          <button onClick={()=>handleRowClick(id)}>NEXT</button>
+     </div>
+           </Card>
+ 
+     </div>
     </HocWrap>
    );
 };
--- src/Components/Produsct/Product.js
+++ src/Components/Produsct/Product.js
@@ -11,6 +11,7 @@ const Product = ({state , loading}) => {
   const handleClick= (record) =>{
 navigate(`/details/${record.id}`);
   }
+  console.log(state);
   
   return (
     <HocWrap>
